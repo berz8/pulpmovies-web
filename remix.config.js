4656abb920513f1d75978fdb29c4ced715d2a73c
Davide Lombardi <davidelombardi130p@gmail.com>
2023-05-09 18:52:52 +0200

    build: added Dockerfile

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   serverModuleFormat: "cjs",
   future: {
