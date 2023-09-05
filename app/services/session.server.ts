789df117ed082d78f8469629456c8285ad1890a5
Davide Lombardi <davidelombardi130p@gmail.com>
2023-09-05 17:56:47 +0200

    fix: google auth on ios, hope it works

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,15 +1,14 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 
-// export the whole sessionStorage object
 export let sessionStorage = createCookieSessionStorage({
   cookie: {
-    name: "_session", // use any name you want here
+    name: "_session",
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
-    secrets: [process.env.COOKIE_SECRET], // replace this with an actual secret
+    secrets: [process.env.COOKIE_SECRET],
     secure: process.env.NODE_ENV === "production", // enable this in prod only
-    maxAge: 60 * 60 * 24 * 365,
+    maxAge: 60 * 60 * 24 * 5,
   },
 });
 
